376bed218a35f0353f68bc697094cd78c479d246
unknown <junqiwu02@gmail.com>
2020-10-24 19:34:11 -0400

    added example markers for each room

--- maps.js
+++ maps.js
@@ -1,8 +1,9 @@
 
+let map;
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 0, lng: 0 },
         zoom: 1,
         streetViewControl: false,
@@ -12,7 +13,7 @@ function initMap() {
     });
     const floorMapType = new google.maps.ImageMapType({
         getTileUrl: function (coord, zoom) {
-            if(!inBounds(zoom, coord.x, coord.y)) {
+            if (!inBounds(zoom, coord.x, coord.y)) {
                 return "";
             }
 
@@ -30,50 +31,67 @@ function initMap() {
         tileSize: new google.maps.Size(512, 512),
         maxZoom: 2,
         minZoom: 1,
-        radius: 1392,
+        radius: 1000,
         name: "Floor",
     });
     map.mapTypes.set("floor", floorMapType);
     map.setMapTypeId("floor");
 
-    const contentString =
-        '<div id="content">' +
-        '<div id="siteNotice">' +
-        "</div>" +
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-        '<div id="bodyContent">' +
-        "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-        "sandstone rock formation in the southern part of the " +
-        "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-        "south west of the nearest large town, Alice Springs; 450&#160;km " +
-        "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-        "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-        "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-        "Aboriginal people of the area. It has many springs, waterholes, " +
-        "rock caves and ancient paintings. Uluru is listed as a World " +
-        "Heritage Site.</p>" +
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-        "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-        "(last visited June 22, 2009).</p>" +
-        "</div>" +
-        "</div>";
-    const infowindow = new google.maps.InfoWindow({
-        content: contentString,
-    });
-    const marker = new google.maps.Marker({
-        position: { lat: 0, lng: 0 },
+    const infowindow = new google.maps.InfoWindow();
+
+    const marker301 = new google.maps.Marker({
+        position: { lat: 65, lng: -135 },
         map,
-        title: "Uluru (Ayers Rock)",
+        title: "Room 301",
     });
-    marker.addListener("click", () => {
-        infowindow.open(map, marker);
+    const marker302 = new google.maps.Marker({
+        position: { lat: 65, lng: -50 },
+        map,
+        title: "Room 302",
+    });
+    const marker303 = new google.maps.Marker({
+        position: { lat: 65, lng: 20 },
+        map,
+        title: "Room 303",
     });
 
-    const infoWindowContent = "<div style=\"background-color:red; padding:10px;\"><h1>Room 305</h1><h2>Capcity 5/5 (FULL)</h2></div>";
-    infowindow.setContent(infoWindowContent)
+    const marker311 = new google.maps.Marker({
+        position: { lat: -80, lng: -135 },
+        map,
+        title: "Room 311",
+    });
+    const marker312 = new google.maps.Marker({
+        position: { lat: -80, lng: -50 },
+        map,
+        title: "Room 312",
+    });
+    const marker313 = new google.maps.Marker({
+        position: { lat: -80, lng: 20 },
+        map,
+        title: "Room 313",
+    });
+
+
+
+    marker301.addListener("click", () => openWindow(infowindow, marker301, 0, 5));
+    marker302.addListener("click", () => openWindow(infowindow, marker302, 1, 5));
+    marker303.addListener("click", () => openWindow(infowindow, marker303, 5, 5));
+
+    marker311.addListener("click", () => openWindow(infowindow, marker311, 3, 3));
+    marker312.addListener("click", () => openWindow(infowindow, marker312, 1, 3));
+    marker313.addListener("click", () => openWindow(infowindow, marker313, 2, 3));
+
 }
 
 function inBounds(zoom, x, y) {
     bounds = 1 << (zoom - 1);
     return 0 <= x && x < bounds && 0 <= y && y < bounds;
+}
+
+function openWindow(infowindow, marker, capacity, max) {
+    let bkgcolor = capacity < max ? "white" : "red";
+    let full = capacity < max ? "" : "(FULL)";
+    const content = `<div style="background-color:${bkgcolor};padding:10px;"><h1>${marker.title}</h1><h2>Capcity ${capacity}/${max} ${full}</h2></div>`;
+    infowindow.setContent(content);
+    infowindow.open(map, marker);
 }
