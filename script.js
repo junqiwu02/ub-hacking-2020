0c575e4fc957da4aacebb2db539bf9ffb3275e81
unknown <Syahrul@ARULRINABITAMI9.sw-home.net>
2020-10-24 17:48:47 -0400

    fixed tiling

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-/*
-let map;
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
-*/
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 0, lng: 0 },
@@ -19,45 +10,26 @@ function initMap() {
   });
   const floorMapType = new google.maps.ImageMapType({
     getTileUrl: function (coord, zoom) {
-      const normalizedCoord = getNormalizedCoord(coord, zoom);
+      console.log(coord + " " + zoom);
 
-      if (!normalizedCoord) {
-        return "";
-      }
-      const bound = Math.pow(2, zoom);
+
       return (
-        "floorplantest" +
-
+        "floorplan" +
+        "-" +
+        zoom +
+        "-" +
+        coord.y +
+        "-" +
+        coord.x +
         ".jpg"
       );
     },
-    tileSize: new google.maps.Size(256, 256),
-    maxZoom: 9,
-    minZoom: 0,
-    radius: 256,
+    tileSize: new google.maps.Size(512, 512),
+    maxZoom: 2,
+    minZoom: 1,
+    radius: 1392,
     name: "Floor",
   });
   map.mapTypes.set("floor", floorMapType);
   map.setMapTypeId("floor");
-}
-
-// Normalizes the coords that tiles repeat across the x axis (horizontally)
-// like the standard Google map tiles.
-function getNormalizedCoord(coord, zoom) {
-  const y = coord.y;
-  let x = coord.x;
-  // tile range in one direction range is dependent on zoom level
-  // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
-  const tileRange = 1 << zoom;
-
-  // don't repeat across y-axis (vertically)
-  if (y < 0 || y >= tileRange) {
-    return null;
-  }
-
-  // repeat across x-axis
-  if (x < 0 || x >= tileRange) {
-    x = ((x % tileRange) + tileRange) % tileRange;
-  }
-  return { x: x, y: y };
 }
