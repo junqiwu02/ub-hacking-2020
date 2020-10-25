59aea2f1504284395a53d2d9c21391f4ed348ebe
unknown <junqiwu02@gmail.com>
2020-10-24 20:02:56 -0400

    fixed map repeating

--- maps.js
+++ maps.js
@@ -4,8 +4,8 @@ let map;
 // When the user clicks the marker, an info window opens.
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 0, lng: 0 },
-        zoom: 1,
+        center: { lat: 65, lng: -90 },
+        zoom: 2,
         streetViewControl: false,
         mapTypeControlOptions: {
             mapTypeIds: ["floor"],
@@ -29,8 +29,8 @@ function initMap() {
             );
         },
         tileSize: new google.maps.Size(512, 512),
-        maxZoom: 2,
-        minZoom: 1,
+        maxZoom: 3,
+        minZoom: 2,
         radius: 1000,
         name: "Floor",
     });
@@ -40,33 +40,33 @@ function initMap() {
     const infowindow = new google.maps.InfoWindow();
 
     const marker301 = new google.maps.Marker({
-        position: { lat: 65, lng: -135 },
+        position: { lat: 79, lng: -160 },
         map,
         title: "Room 301",
     });
     const marker302 = new google.maps.Marker({
-        position: { lat: 65, lng: -50 },
+        position: { lat: 79, lng: -115 },
         map,
         title: "Room 302",
     });
     const marker303 = new google.maps.Marker({
-        position: { lat: 65, lng: 20 },
+        position: { lat: 79, lng: -80 },
         map,
         title: "Room 303",
     });
 
     const marker311 = new google.maps.Marker({
-        position: { lat: -80, lng: -135 },
+        position: { lat: 20, lng: -160 },
         map,
         title: "Room 311",
     });
     const marker312 = new google.maps.Marker({
-        position: { lat: -80, lng: -50 },
+        position: { lat: 20, lng: -115 },
         map,
         title: "Room 312",
     });
     const marker313 = new google.maps.Marker({
-        position: { lat: -80, lng: 20 },
+        position: { lat: 20, lng: -80 },
         map,
         title: "Room 313",
     });
@@ -84,14 +84,14 @@ function initMap() {
 }
 
 function inBounds(zoom, x, y) {
-    bounds = 1 << (zoom - 1);
+    bounds = 1 << (zoom - 2);
     return 0 <= x && x < bounds && 0 <= y && y < bounds;
 }
 
 function openWindow(infowindow, marker, capacity, max) {
-    let bkgcolor = capacity < max ? "white" : "red";
+    let bkgcolor = capacity < max ? "white" : "tomato";
     let full = capacity < max ? "" : "(FULL)";
-    const content = `<div style="background-color:${bkgcolor};padding:10px;"><h1>${marker.title}</h1><h2>Capcity ${capacity}/${max} ${full}</h2></div>`;
+    const content = `<div style="background-color:${bkgcolor};padding:10px;"><h1>${marker.title}</h1><h2 style="font-weight:lighter"><i>Capcity ${capacity}/${max} ${full}</i></h2></div>`;
     infowindow.setContent(content);
     infowindow.open(map, marker);
 }
