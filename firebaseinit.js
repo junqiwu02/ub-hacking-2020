df27fbe8fc2da319a22a3009be63ec8fd00d35f8
unknown <Syahrul@ARULRINABITAMI9.sw-home.net>
2020-10-25 03:46:01 -0400

    fixed colored markers

--- firebaseinit.js
+++ firebaseinit.js
@@ -11,13 +11,20 @@ function update() {
          if (rooms[index].name == arrayDatabase[indexOuter][1].name) {
            rooms[index].capacity = arrayDatabase[indexOuter][1].capacity;
            rooms[index].max = arrayDatabase[indexOuter][1].max;
+           if (rooms[index].capacity < rooms[index].max) {
+             rooms[index].marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png')
+           } else {
+             rooms[index].marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
+
+           }
+
          }
         }
       }
-
+      //marker101.setIcon('green-dot.png')
     // test: console.log(snapshot.val())
    });
-   //test console.log(rooms)
+   //console.log(rooms)
 
    setTimeout(update,5000)
 }
