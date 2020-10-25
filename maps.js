b24c50d4dd25d7d5c14fa736a4183b19460b182c
unknown <junqiwu02@gmail.com>
2020-10-25 00:08:01 -0400

    added room images and floor 2 rooms

--- maps.js
+++ maps.js
@@ -1,5 +1,6 @@
 
 let map;
+let infowindow;
 let rooms = [];
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
@@ -46,37 +47,52 @@ function initMap() {
     map.mapTypes.set("floor2", floor2MapType);
     map.setMapTypeId("floor1");
 
-    const infowindow = new google.maps.InfoWindow();
+    infowindow = new google.maps.InfoWindow();
 
-    const marker101 = new google.maps.Marker({position: { lat: 79, lng: -160 }, map, title: "Room 101"});
-    const marker102 = new google.maps.Marker({position: { lat: 79, lng: -115 }, map, title: "Room 102"});
-    const marker103 = new google.maps.Marker({position: { lat: 79, lng: -80 }, map, title: "Room 103"});
-    const marker111 = new google.maps.Marker({position: { lat: 20, lng: -160 }, map, title: "Room 111"});
-    const marker112 = new google.maps.Marker({position: { lat: 20, lng: -115 }, map, title: "Room 112"});
-    const marker113 = new google.maps.Marker({position: { lat: 20, lng: -80 }, map, title: "Room 113"});
+    const marker101 = new google.maps.Marker({ position: { lat: 79, lng: -160 }, map, title: "Room 101" });
+    const marker102 = new google.maps.Marker({ position: { lat: 79, lng: -115 }, map, title: "Room 102" });
+    const marker103 = new google.maps.Marker({ position: { lat: 79, lng: -80 }, map, title: "Room 103" });
+    const marker111 = new google.maps.Marker({ position: { lat: 20, lng: -160 }, map, title: "Room 111" });
+    const marker112 = new google.maps.Marker({ position: { lat: 20, lng: -115 }, map, title: "Room 112" });
+    const marker113 = new google.maps.Marker({ position: { lat: 20, lng: -80 }, map, title: "Room 113" });
 
     const floor1Markers = [marker101, marker102, marker103, marker111, marker112, marker113];
-    floor1Markers.forEach(m => {
-        const r = {
-            name: m.title,
-            capacity: 0,
-            max: 5,
-            marker: m
-        };
-        rooms.push(r);
-        m.addListener("click", () => openWindow(infowindow, r));
-    });
+    floor1Markers.forEach(m => setupRoom(m, m.title.includes("3") ? "door2.jpg" : "door1.jpg"));
+
+    const markerGym = new google.maps.Marker({ position: { lat: 60, lng: -115 }, map, title: "Gym" });
+    floor1Markers.push(markerGym);
+    setupRoom(markerGym, "gym.jpg");
+
+    const marker201 = new google.maps.Marker({ position: { lat: 82, lng: -41 }, title: "Room 201" });
+    const marker202 = new google.maps.Marker({ position: { lat: 65, lng: -41 }, title: "Room 202" });
+    const marker203 = new google.maps.Marker({ position: { lat: 20, lng: -41 }, title: "Room 203" });
+    const marker204 = new google.maps.Marker({ position: { lat: 20, lng: -80 }, title: "Room 204" });
+    const marker205 = new google.maps.Marker({ position: { lat: 10, lng: -115 }, title: "Room 205" });
+
+    const floor2Markers = [marker201, marker202, marker203, marker204, marker205];
+    floor2Markers.forEach(m => setupRoom(m, m.title.includes("3") ? "door2.jpg" : "door1.jpg"));
+
+    const markerAuditorium = new google.maps.Marker({ position: { lat: 60, lng: -115 }, title: "Auditorium" });
+    floor2Markers.push(markerAuditorium);
+    setupRoom(markerAuditorium, "auditorium.jpg");
 
     map.addListener("maptypeid_changed", () => {
-        if(map.getMapTypeId() == "floor1") {
+        if (map.getMapTypeId() == "floor1") {
             floor1Markers.forEach(m => {
                 m.setMap(map);
+            });
+            floor2Markers.forEach(m => {
+                m.setMap(null);
             });
         } else {
             floor1Markers.forEach(m => {
                 m.setMap(null);
             });
+            floor2Markers.forEach(m => {
+                m.setMap(map);
+            });
         }
+        infowindow.close();
     });
 }
 
@@ -85,10 +101,22 @@ function inBounds(zoom, x, y) {
     return 0 <= x && x < bounds && 0 <= y && y < bounds;
 }
 
-function openWindow(infowindow, room) {
+function openWindow(room) {
     let bkgcolor = room.capacity < room.max ? "white" : "tomato";
     let full = room.capacity < room.max ? "" : "(FULL)";
-    const content = `<div style="background-color:${bkgcolor};padding:10px;"><h1>${room.name}</h1><h2 style="font-weight:lighter"><i>Capcity ${room.capacity}/${room.max} ${full}</i></h2></div>`;
+    const content = `<div style="background-color:${bkgcolor};padding:10px;"><h1>${room.name}</h1><h2 style="font-weight:lighter"><i>Capcity ${room.capacity}/${room.max} ${full}</i></h2><img src="images/${room.img}" width="120px" height="auto"></div>`;
     infowindow.setContent(content);
     infowindow.open(map, room.marker);
+}
+
+function setupRoom(marker, imageUrl) {
+    const r = {
+        name: marker.title,
+        capacity: 0,
+        max: 1,
+        marker: marker,
+        img: imageUrl
+    };
+    rooms.push(r);
+    marker.addListener("click", () => openWindow(r));
 }
